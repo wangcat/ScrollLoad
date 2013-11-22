3d44b997e8fb3c26da56fc48e50fcb5a46e70ad7
wjw <w.j.w@live.cn>
2013-11-22 11:18:30 +0800

    init

---
+++ ScrollLoad.js
@@ -0,0 +1,23 @@
+﻿(function ($) {
+    var scrollloading = false;
+    $.fn.extend({
+        scrollload: function (callback) {
+            var el = $(this);
+            $(window).bind("scroll", function () {
+                //动态递减数参数
+                if ($(window).scrollTop() > parseInt(el.offset().top) + el.height() - $(window).height()) {
+                    if (scrollloading) return;
+
+                    if (typeof callback == "function") {
+                        scrollloading = true;
+                        callback();
+                    }
+                    //避免重复加载
+                    var stop = setTimeout(function () {
+                        scrollloading = false;
+                    }, 2000);
+                }
+            });
+        }
+    });
+})(jQuery);
